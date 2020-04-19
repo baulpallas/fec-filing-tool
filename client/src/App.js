c8fe576764300ed76b0fc7054b9cfafc4519450a
Paul Ballas <paballas51@gmail.com>
2020-04-19 00:56:34 -0400

    feature: got comp to old working

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,16 @@ import axios from "axios";
 function App() {
   const [val, setVal] = useState(null);
   const apiCall = async (fecNum) => {
+    let newNum;
     setInterval(async () => {
       const data = await axios.get(
         `https://api.open.fec.gov/v1/committee/${fecNum}/filings/?sort_null_only=false&page=1&sort_nulls_last=false&api_key=${process.env.REACT_APP_FEC_KEY}&per_page=20&sort_hide_null=false&election_full=true`
       );
-      console.log(data.data.results);
+      newNum = data.data.results[0].fec_file_id;
+      localStorage.setItem("Most_Recent_Filing", newNum);
+      let oldNum = localStorage.getItem("Most_Recent_Filing");
+      if (oldNum === newNum) {
+      }
     }, 5000);
   };
 
