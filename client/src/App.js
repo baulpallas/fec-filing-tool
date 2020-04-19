1041daffad47d3e3e9745649cce11bf2b9bf9703
Paul Ballas <paballas51@gmail.com>
2020-04-19 00:30:49 -0400

    feature: api call works

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
 
 function App() {
+  const [val, setVal] = useState(null);
+  const apiCall = async (fecNum) => {
+    setInterval(async () => {
+      const data = await axios.get(
+        `https://api.open.fec.gov/v1/committee/${fecNum}/filings/?sort_null_only=false&page=1&sort_nulls_last=false&api_key=${process.env.REACT_APP_FEC_KEY}&per_page=20&sort_hide_null=false&election_full=true`
+      );
+      console.log(data.data.results);
+    }, 5000);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    apiCall(val);
+  };
+
+  const handleChange = (e) => {
+    setVal(e.target.value);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label for="exampleInputEmail1">Enter FEC Committee Number Here:</label>
+      <div class="form-group">
+        <label for="exampleInputEmail1">
+          Enter the FEC Committee Number here:{" "}
+        </label>
         <input
           type="text"
-          className="form-control"
+          class="form-control"
           id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
+          onChange={handleChange}
+          placeholder="FEC Committee Number"
         />
       </div>
-      <button type="submit" className="btn btn-primary">
+      <div>{}</div>
+      <button type="submit" class="btn btn-primary">
         Submit
       </button>
     </form>
